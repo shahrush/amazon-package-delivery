e4e81360e093851bef3d67e34a17ca25c850cf5e
Rushabh <shahrushabh1994@gmail.com>
2018-12-20 14:41:13 -0500

    main driver

---
+++ src/main/java/Main.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class Main extends DataLoader {
+
+
+    private static List<List<String>> preProcessingData(List<String> data){
+
+        List<String> packages = data.stream().map(s-> s.replaceAll("\\{}", "").
+                replaceAll("}", "").replaceAll("\"", "")).collect(Collectors.toList());
+
+        List<List<String>> attributes = packages.stream().map(s -> Arrays.asList(s.split(","))).collect(Collectors.toList());
+
+        return attributes;
+    }
+
+    private static boolean isEmptyValue(List<String> entry){
+        for(String e:entry){
+            if(e.split(":").length<2){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        DataLoader d = new DataLoader();
+        List<String> data = d.GetData();
+        List<PackageClass> packageClassList = new ArrayList<>();
+        List<PackageClass> fragilePackages;
+        List<PackageClass> expeditedPackages;
+        List<PackageClass> normalPackages;
+        List<List<String>> attributes;
+
+        attributes = preProcessingData(data);
+
+        for(List<String> attribute: attributes) {
+
+            if (!isEmptyValue(attribute)){
+                packageClassList.add(new PackageClass(attribute));
+            }
+        }
+    }
+}
